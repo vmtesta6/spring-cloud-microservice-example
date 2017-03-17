db267abd9a8c8cb5a64ccb7f155546208a546d79
sailakshmiv <lakshmi.saiv99@gmail.com>
2017-03-17 14:20:24 +0530

    #VMSCRUM-4

--- api-gateway-microservice/src/main/java/services/Application.java
+++ api-gateway-microservice/src/main/java/services/Application.java
@@ -1,9 +1,11 @@
 package services;
 
+//packages
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 
+//class
 @SpringBootApplication
 @EnableSidecar
 public class Application {

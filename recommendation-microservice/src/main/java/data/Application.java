1822ec0b691f18dded34d081b229daac9c7e267b
sailakshmiv <lakshmi.saiv99@gmail.com>
2017-03-17 14:29:48 +0530

    #VMSCRUM-4

--- recommendation-microservice/src/main/java/data/Application.java
+++ recommendation-microservice/src/main/java/data/Application.java
@@ -1,6 +1,6 @@
 package data;
 
-//packages
+//packages1
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.springframework.boot.SpringApplication;
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 
-//class
+//class1
 @SpringBootApplication
 @EnableNeo4jRepositories
 @EnableDiscoveryClient

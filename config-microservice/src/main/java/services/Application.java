f619fdf4a68f0735a64087098a3786deb35e8794
vmtesta6 <mkveepuru@gmail.com>
2017-03-20 13:07:23 +0530

    #VMSCRUM-2

--- config-microservice/src/main/java/services/Application.java
+++ config-microservice/src/main/java/services/Application.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
+//config-microservice
 @Configuration
 @EnableAutoConfiguration
 @EnableDiscoveryClient
